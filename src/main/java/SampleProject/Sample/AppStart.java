bf825bd3fe9845b6f90ce8e523dfef18b7e03c92
Rishi <rishi.dareme@gmail.com>
2017-10-24 07:04:25 +0530

    second commit

---
+++ src/main/java/SampleProject/Sample/AppStart.java
@@ -0,0 +1,16 @@
+package SampleProject.Sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class AppStart
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        SampleMain sm = new SampleMain();
+        //Print the sum.
+        System.out.println(sm.add());
+    }
+}
---
+++ src/main/java/SampleProject/Sample/SampleMain.java
@@ -0,0 +1,10 @@
+package SampleProject.Sample;
+
+public class SampleMain {
+
+	public int add(){
+		int a = 5;
+		int b = 5;
+		return a+b;
+	}
+}
